9f3af30be507bb8b16ac15fe8fbcaadf87457480
CJT1105 <CJT1105@GMAIL.COM>
2016-07-21 20:35:28 -0500

    added function that checks if user is logged in to control if login button is shown or not

--- controllers/NavCtrl.js
+++ controllers/NavCtrl.js
@@ -1,6 +1,39 @@
-app.controller("NavCtrl", function($scope, userFactory){
-	// $scope.checkIfLoggedIn = userFactory.checkIfLoggedIn;
-	// $scope.checkIfLoggedIn();
+app.controller("NavCtrl", function($scope, userFactory, $q){
 	$scope.login = userFactory.login;
+	$scope.logout = function(){
+		firebase.auth().signOut().then(function(){
+			$scope.checkIfLoggedIn();
+			console.log('Signed Out');
+		})
+	}
+	$scope.loggedIn = "hey";
+	$scope.loggedIn = userFactory.checkIfLoggedIn();
+	// console.log('jnrfrfnrjfn', userFactory.checkIfLoggedIn)
+	console.log('scop',userFactory.checkIfLoggedIn() )
 
+
+// 	checkIfLoggedIn($scope);
+// 	function checkIfLoggedIn($scope){
+// 		$q(function(resolve, reject){
+// 			let user = firebase.auth().currentUser;
+// 			resolve(user);
+// 		}).then(function(user){
+// 			console.log(user);
+// 			if (user){
+// 				loggedin = true;
+// 				console.log(loggedin)
+// 				return loggedin
+// 			}
+// 			else {
+// 				let loggedin = false;
+// 				console.log(loggedin)
+// 				return loggedin
+
+// 			}
+// 		}).then(function(loggedin){
+// 			$scope.loggedIn = loggedin
+// 		})
+// 	}
+
+// return checkIfLoggedIn
 });
--- factories/UserFactory.js
+++ factories/UserFactory.js
@@ -1,27 +1,46 @@
-app.factory('userFactory', function(){
+app.factory('userFactory', function($q){
+
 	let login = function(){
-		let config = {
-    apiKey: "AIzaSyCvFEbvfcRcano8q8kxmsDltQ8nm9w9ZM0",
-    authDomain: "cap-test-77b43.firebaseapp.com",
-    databaseURL: "https://cap-test-77b43.firebaseio.com",
-    storageBucket: "",
-		};
-		firebase.initializeApp(config);
 		let provider = new firebase.auth.FacebookAuthProvider();
-		firebase.auth().signInWithPopup(provider);
-	}
-	let checkIfLoggedIn = function(){
-		firebase.auth().onAuthStateChanged(function(user) {
-			if (user) {
- 			let currentUser = user;
+		firebase.auth().signInWithPopup(provider).then(function(){
+			checkIfLoggedIn()
+		})
+	};
+
+	function checkIfLoggedIn() {
+			$q(function(resolve, reject){
+				let user = firebase.auth().currentUser;
+				if (user){
+				loggedin = true
+				resolve(loggedin)
 			}
 			else {
-    		alert("not signed in")
+				loggedin = false;
+				resolve(loggedin)
 			}
-		});
-	}
+			}).then(function(loggedin){
+				return loggedin
+			})
+			return loggedin
+		}
+			// if (user){
+			// 	loggedin = true;
+			// 	console.log(loggedin)
+			// }
+			// else {
+			// 	loggedin = false;
+			// 	console.log(loggedin)
+
+			// }
+		// })
+		// console.log('this my shit', loggedin)
+		// return loggedin
+	// }
+
+
 	let getCurrentUser = function(){
-		return firebase.auth().cu
-	}
-	return {login, }
-})
+		let user = firebase.auth().currentUser;
+	};
+	return {login, checkIfLoggedIn, getCurrentUser}
+	})
+
